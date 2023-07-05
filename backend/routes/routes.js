652a39c9da89260f8e7d9f78f169f27f19082aee
Albert <87812438+AlbertRT@users.noreply.github.com>
2023-07-05 08:28:13 +0700

    Reading users folder function

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import { login, logout, register } from '../controllers/UserController.js';
 import { cookieValidation } from '../middleware/cookieValidation.js';
+import { readFolder } from '../controllers/FilesController.js';
 
 const Route = express.Router();
 
 // Authentication routes
 Route.post('/user/register', register);
 Route.post('/user/login', login);
-Route.delete('/user/logout', cookieValidation, logout)
+Route.delete('/user/logout', cookieValidation, logout);
+
+// Files manager routes
+
+// folders manager
+Route.post('/user/file/folder/create');
+Route.patch('/user/file/folder/rename');
+Route.delete('/user/file/folder/delete');
+Route.get('/user/file',cookieValidation, readFolder);
 
 
 Route.get('/', cookieValidation, (req, res) => res.json({ ok: true }));
--- backend/utils/fs.js
+++ backend/utils/fs.js
@@ -1,11 +1,52 @@
-import fs from 'fs/promises';
+import fs from 'fs';
+import path from 'path';
 
 export async function createFolder(path) {
     try {
-        await fs.mkdir(path);
-        
+        await fs.promises.mkdir(path);
+
         return 1;
     } catch (error) {
         return error.message;
+    }
+}
+
+function getItemInfo(itemPath) {
+    return new Promise((resolve, reject) => {
+        fs.stat(itemPath, (err, stats) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            const type = stats.isDirectory() ? 'folder' : `file/${path.basename(itemPath).split('.')[1]}`;
+
+            const item = {
+                type,
+                name: path.basename(itemPath),
+                created: stats.birthtimeMs,
+                size: stats.size
+            };
+
+            resolve(item);
+        });
+    });
+}
+
+export async function readDir(folderPath) {
+    try {
+        const files = await fs.promises.readdir(folderPath);
+        const items = [];
+
+        for (const file of files) {
+            const itemPath = path.join(folderPath, file);
+            const item = await getItemInfo(itemPath);
+            items.push(item);
+        }
+        console.log(items);
+
+        return items;
+    } catch (err) {
+        return err;
     }
 }
